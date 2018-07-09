ce02d1ad129a833411d3239017331c411bed912b
18035 <1979646055@qq.com>
2018-07-09 09:44:47 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    tables:[
+      {text: '中国健康与养⽼追踪调查', id: 'tb1'},
+      { text: '测试问卷2', id: 'tb2'},
+      { text: '测试问卷3', id: 'tb3'},
+      { text: '测试问卷4', id: 'tb4'}
+    ]
+
+    
+    
+  },
+  //事件处理函数
+  bindViewTap: function(event) {
+    wx.navigateTo({
+      url: '../tables/' + event.target.id + '/' + event.target.id,
+    })
+  },
+
+ 
+})
---
+++ pages/tables/tb1/tb1.js
@@ -0,0 +1,74 @@
+// pages/tables/tb1/tb1.js
+Page({
+
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title : '中国健康与养老追踪调查',
+    radios:[
+      {radio:'家庭住宅'},
+      {radio:'养老院及其他机构'},
+      {radio:'医院'},
+      {radio:'其他'}
+    ]
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
