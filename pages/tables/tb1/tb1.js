a1029efb4f82c27a1e6d9fe868b0bde57f0b35f4
18035 <1979646055@qq.com>
2018-07-10 23:22:03 +0800

    fix bugs

--- pages/tables/tb1/tb1.js
+++ pages/tables/tb1/tb1.js
@@ -10,12 +10,70 @@ Page({
     showTopTips : false,
     TopTipsText : '',
     forms:[
-      {qid:1,order:1,formType:1,qText:'您所居住的社区是？',radio:{A:'家庭住宅',B:'养老院及其他机构',C:'医院',D:'其他'}},
-      {qid:2,radio:'养老院及其他机构'},
-      {radio:'医院'},
-      {radio:'其他'}
+      {
+        qid: 1, order: 1, formType: '2', qText: '您所居住的社区是？', radioItems: [
+          { name: '家庭住宅', value: '0' },
+          { name: '养老院及其他机构', value: '1', checked: true },
+          { name: '医院', value: '2'},
+          { name: '其他', value: '3'}
+        ]
+      },
+       {
+        qid: 2, order: 2, formType: '1', qText: '您所居住的社区是？', radioItems: [
+          { name: '家庭住宅', value: '0' },
+          { name: '养老院及其他机构', value: '1', checked: true },
+          { name: '医院', value: '2' },
+          { name: '其他', value: '3' }
+        ]
+      }, {
+        qid: 3, order: 3, formType: '3', qText: '您所居住的社区是？', radioItems: [
+          { name: '家庭住宅', value: '0' },
+          { name: '养老院及其他机构', value: '1', checked: true },
+          { name: '医院', value: '2' },
+          { name: '其他', value: '3' }
+        ]
+      }
+     
     ]
   
+  },
+  radioChange: function (e) {
+    var value = e.detail.value;
+    console.log(this.data);
+    console.log('radio发生change事件，携带value值为：', value);
+
+    var radioItems = this.data.forms[e.target.id].radioItems;
+    console.log(radioItems);
+    
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == value;
+    }
+    
+    
+    this.setData({
+      radioItems: radioItems
+    });
+  }, 
+
+  
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
   },
 
   /**
