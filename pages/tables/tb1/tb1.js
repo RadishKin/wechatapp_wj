ca31dfbc2856266b2cf13149133abfb48f391ff7
18035 <1979646055@qq.com>
2018-07-11 09:52:19 +0800

    fix bugs

--- pages/tables/tb1/tb1.js
+++ pages/tables/tb1/tb1.js
@@ -7,11 +7,12 @@ Page({
    */
   data: {
     title : '中国健康与养老追踪调查',
+    isAgree: false,
     showTopTips : false,
     TopTipsText : '',
     forms:[
       {
-        qid: 1, order: 1, formType: '2', qText: '您所居住的社区是？', radioItems: [
+        qid: 1, order: 1, formType: '4', qText: '您所居住的社区是？',  radioItems: [
           { name: '家庭住宅', value: '0' },
           { name: '养老院及其他机构', value: '1', checked: true },
           { name: '医院', value: '2'},
@@ -19,11 +20,11 @@ Page({
         ]
       },
        {
-        qid: 2, order: 2, formType: '1', qText: '您所居住的社区是？', radioItems: [
-          { name: '家庭住宅', value: '0' },
-          { name: '养老院及其他机构', value: '1', checked: true },
-          { name: '医院', value: '2' },
-          { name: '其他', value: '3' }
+        qid: 2, order: 2, formType: '1', qText: '您所居住的社区是2？', radioItems: [
+          { name: '家庭住宅2', value: '0' },
+          { name: '养老院及其他机构2', value: '1', checked: true },
+          { name: '医院2', value: '2' },
+          { name: '其他2', value: '3' }
         ]
       }, {
         qid: 3, order: 3, formType: '3', qText: '您所居住的社区是？', radioItems: [
@@ -39,27 +40,30 @@ Page({
   },
   radioChange: function (e) {
     var value = e.detail.value;
-    console.log(this.data);
+    let id = e.target.id - 1
+
     console.log('radio发生change事件，携带value值为：', value);
 
-    var radioItems = this.data.forms[e.target.id].radioItems;
-    console.log(radioItems);
+    var radioItems = this.data.forms[id].radioItems;
+    
     
     for (var i = 0, len = radioItems.length; i < len; ++i) {
       radioItems[i].checked = radioItems[i].value == value;
     }
     
     
+    let form = 'forms[' + id + '].radioItems'
     this.setData({
-      radioItems: radioItems
+      [form] : radioItems
     });
   }, 
 
   
   checkboxChange: function (e) {
+    let id = e.target.id - 1
     console.log('checkbox发生change事件，携带value值为：', e.detail.value);
 
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    var checkboxItems = this.data.forms[id].radioItems, values = e.detail.value;
     for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
       checkboxItems[i].checked = false;
 
@@ -70,11 +74,47 @@ Page({
         }
       }
     }
-
+    let checkbox = 'forms[' + id + '].radioItems'
+    console.log(checkbox)
     this.setData({
-      checkboxItems: checkboxItems
+      [checkbox] : checkboxItems
     });
   },
+
+  bindAgreeChange: function (e) {
+    this.setData({
+      isAgree: !!e.detail.value.length
+    });
+  },
+  uploadForm:function(e){
+    var forms = JSON.stringify(this.data.forms)
+    wx.request({
+      url: 'https://adc.com/settables',
+      method: 'POST',
+      data: {
+        data:forms
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        if (statusCode == 200) {
+          wx.navigateTo({
+            url: 'msg_success'
+          })
+        } else {
+          wx.navigateTo({
+            url: 'msg_fail'
+          })
+        }
+
+      }
+
+    })
+    
+      
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -92,7 +132,7 @@ Page({
         },
         success: function (res) {
           if (statusCode == 200) {
-            this.data.tables = res.data
+            this.data.forms = res.data
           }else{
             this.data.showTopTips = true;
             this.data.TopTipsText = '问卷拉取失败，请下拉刷新'
